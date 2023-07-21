bc35232648706bd941d97939159cd614ade386dd
LeonardoCBoar <43409820+LeonardoCBoar@users.noreply.github.com>
2023-07-21 11:48:49 -0300

    Add files via upload

--- src/card_separator.py
+++ src/card_separator.py
@@ -1,7 +1,42 @@
 import numpy
+import cv2
+
+# TODO: Parametrize in a config menu
+THRESHOLD1 = 23
+THRESOLD2 = 20
+
+AREA_THRESOLD = 5000
 
 class CardSeparator():
-    def __init__(self):
+    def __init__(self) -> None:
+        pass
+
+    def separate_card(self, frame: numpy.ndarray, mouse_x: int, mouse_y: int) -> numpy.ndarray:
+        pass
+
+    def preprocess_image(image: numpy.ndarray) -> numpy.ndarray:
+        """Apply 7x7 gaussian blur on image, then return it on gray scale"""
+        blurred_image = cv2.GaussianBlur(image, (7, 7), 1)
+        gray_image = cv2.cvtColor(blurred_image, cv2.COLOR_BGR2GRAY)
+
+    
+    def get_image_contours(self, preprocessed_image: numpy.ndarray):
+        """Return the contours of all objects with area > AREA_THRESOLD """
+        threshold1 = 23
+        threshold2 = 20
+        
+        # Find edges around of image on pos
+        canny_image = cv2.Canny(preprocessed_image, threshold1, threshold2)
+
+        kernel = numpy.ones((5, 5))
+        dilatated_image = cv2.dilate(canny_image, kernel, iterations=1)
+
+        all_contours, _ = cv2.findContours(dilatated_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        filtered_contours: list = []
+        return filtered_contours 
+
+    def get_contour_around_point(self, contours, x: int, y: int):
         pass
         
     cap = cv2.VideoCapture(0)
@@ -30,36 +65,6 @@ class CardSeparator():
         else:
             print("Nenhum objeto válido capturado.")
     
-    def stackImages(scale,imgArray):
-        rows = len(imgArray)
-        cols = len(imgArray[0])
-        rowsAvailable = isinstance(imgArray[0], list)
-        width = imgArray[0][0].shape[1]
-        height = imgArray[0][0].shape[0]
-        if rowsAvailable:
-            for x in range ( 0, rows):
-                for y in range(0, cols):
-                    if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
-                        imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
-                    else:
-                        imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
-                    if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
-            imageBlank = np.zeros((height, width, 3), np.uint8)
-            hor = [imageBlank]*rows
-            hor_con = [imageBlank]*rows
-            for x in range(0, rows):
-                hor[x] = np.hstack(imgArray[x])
-            ver = np.vstack(hor)
-        else:
-            for x in range(0, rows):
-                if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-                    imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
-                else:
-                    imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
-                if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-            hor= np.hstack(imgArray)
-            ver = hor
-        return ver
     
     def getContours(img,imgContour):
         contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
@@ -110,7 +115,5 @@ class CardSeparator():
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     
-    def separate_card(self, frame: numpy.ndarray, mouse_x: int, mouse_y: int) -> numpy.ndarray:
-        pass
 
     
