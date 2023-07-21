975c2d75ffed0b940db7ec4303892ca25d6b2f9d
LeonardoCBoar <43409820+LeonardoCBoar@users.noreply.github.com>
2023-07-21 10:58:29 -0300

    Added mouse click detection

--- src/interface_manager.py
+++ src/interface_manager.py
@@ -1,4 +1,5 @@
 import sys
+import numpy
 
 from PyQt6 import QtWidgets
 from PyQt6 import QtCore
@@ -11,10 +12,12 @@ from card_separator import CardSeparator
 
 class InterfaceManager():
     def __init__(self) -> None:
+        # Configure camera capture
         self.camera = cv2.VideoCapture(0)
 
         self.window_setup()
 
+        # Timer to update camera image
         self.update_timer = QtCore.QTimer()
         self.update_timer.timeout.connect(self.update)
         self.update_timer.start(33)
@@ -34,27 +37,34 @@ class InterfaceManager():
         main_widget.setLayout(main_layout)
         self.main_window.setCentralWidget(main_widget)
 
+        # Label to display camera output
         self.camera_viewport = QtWidgets.QLabel()
         main_layout.addWidget(self.camera_viewport)
 
+        # Define function to be triggered on mouse clicked
+        self.camera_viewport.mousePressEvent = self.on_mouse_click
+
         self.main_window.show()
 
+    def numpy_image_to_qimage(self, raw_image: numpy.ndarray) -> QtGui.QImage:
+        height, width, _ = raw_image.shape
+        converted_image = QtGui.QImage(raw_image, width, height, QtGui.QImage.Format.Format_BGR888)
+        return converted_image
     
     def update(self) -> None:
         ret, frame = self.camera.read()
-        self.frame = frame
-
-        converted_image = QtGui.QImage(frame, frame.shape[1], frame.shape[0], 
-                                       QtGui.QImage.Format.Format_BGR888)
         
-        self.camera_viewport.setPixmap(QtGui.QPixmap.fromImage(converted_image))
+        camera_image = self.numpy_image_to_qimage(frame)
+
+        self.camera_viewport.setPixmap(QtGui.QPixmap.fromImage(camera_image))
 
     def run(self) -> None:
         sys.exit(self.app.exec())
 
 
-    ## PLACEHOLDER
-    def on_card_click(self, pos_x: int, pos_y: int) -> None:
-        separated_card = self.card_separator.separate_card(self.frame, pos_x, pos_y)
+    def on_mouse_click(self, event: QtGui.QMouseEvent) -> None:
+        x = event.pos().x()
+        y = event.pos().y()
+        #separated_card = self.card_separator.separate_card(self.frame, x, y)
 
-        identified_card = self.card_identificator.identify_card(separated_card)
+        #identified_card = self.card_identificator.identify_card(separated_card)
