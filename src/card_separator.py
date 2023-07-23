5597056a065eb4b85824c57cc88a03c8b2a878a9
LeonardoCBoar <leonardodicredico@gmail.com>
2023-07-23 13:47:36 -0300

    Feat: Organized background removal in the class CardSeparator

--- src/card_separator.py
+++ src/card_separator.py
@@ -12,7 +12,11 @@ class CardSeparator():
         pass
 
     def separate_card(self, frame: numpy.ndarray, mouse_x: int, mouse_y: int) -> numpy.ndarray:
-        pass
+        preprocessed_image = self.preprocess_image(frame)
+        all_contours = self.get_image_contours(preprocessed_image)
+        card_contour = self.get_image_contours(all_contours)
+        separated_card = self.crop_image(frame, card_contour)
+        return separated_card
 
     def preprocess_image(image: numpy.ndarray) -> numpy.ndarray:
         """Apply 7x7 gaussian blur on image, then return it on gray scale"""
@@ -31,89 +35,28 @@ class CardSeparator():
         kernel = numpy.ones((5, 5))
         dilatated_image = cv2.dilate(canny_image, kernel, iterations=1)
 
+        # Get the coordinates from the edges on canny_image
         all_contours, _ = cv2.findContours(dilatated_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         filtered_contours: list = []
+        for contour in all_contours:
+            area = cv2.contourArea(contour)
+            if area > AREA_THRESOLD:
+                filtered_contours.append(contour)
+
         return filtered_contours 
 
-    def get_contour_around_point(self, contours, x: int, y: int):
-        pass
-        
-    cap = cv2.VideoCapture(0)
-    
-    cv2.namedWindow("Parameters")
-    cv2.resizeWindow("Parameters",640,240)
-    cv2.createTrackbar("Threshold1","Parameters",23,255,empty)
-    cv2.createTrackbar("Threshold2","Parameters",20,255,empty)
-    cv2.createTrackbar("Area","Parameters",5000,30000,empty)
-    
-    object_captured = False
-    object_coords = []
-    
-    def on_mouse_click(event, x, y, flags, param):
-        global object_captured, object_coords
-        if event == cv2.EVENT_LBUTTONDOWN:
-            object_captured = True
-            object_coords = (x, y)
-    
-    def show_cropped_image(img, x, y, w, h):
-        object_crop = img[y:y+h, x:x+w]
-    
-        # Verifique se o objeto recortado é válido antes de exibi-lo
-        if object_crop.size != 0:
-            cv2.imshow("Objeto Recortado", object_crop)
-        else:
-            print("Nenhum objeto válido capturado.")
-    
-    
-    def getContours(img,imgContour):
-        contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        for cnt in contours:
-            area = cv2.contourArea(cnt)
-            areaMin = cv2.getTrackbarPos("Area", "Parameters")
-            if area > areaMin:
-                cv2.drawContours(imgContour, cnt, -1, (255, 0, 255), 7)
-                peri = cv2.arcLength(cnt, True)
-                approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
-                x , y , w, h = cv2.boundingRect(approx)
-                cv2.rectangle(imgContour, (x , y ), (x + w , y + h ), (0, 255, 0), 5)
-                
-                
-    cv2.namedWindow("Result")
-    cv2.setMouseCallback("Result", on_mouse_click)
-    
-    while True:
-        success, img = cap.read()
-        imgContour = img.copy()
-        imgBlur = cv2.GaussianBlur(img, (7, 7), 1)
-        imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
-        threshold1 = 23
-        threshold2 = 20
-        imgCanny = cv2.Canny(imgGray, threshold1, threshold2)
-        kernel = np.ones((5, 5))
-        imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
-        #Mostra na janela, os contornos que estão sendo feitos em volta do objeto
-        #getContours(imgDil, imgContour)
-        contours, _ = cv2.findContours(imgDil, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
-        cv2.imshow("Result", imgContour)
-        
-        cv2.setMouseCallback("Result", on_mouse_click)
-    
-        if object_captured:
-            object_captured = False
-            x, y = object_coords
-    
-            # Encontre o contorno correspondente ao objeto clicado
-            for contour in contours:
-                if cv2.pointPolygonTest(contour, (x, y), False) >= 0:
-                    # Obtenha as coordenadas e dimensões do retângulo delimitador do objeto
-                    x, y, w, h = cv2.boundingRect(contour)
-                    show_cropped_image(img, x, y, w, h)
-                    break
-    
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    
+    def get_contour_around_point(self, all_contours, x: int, y: int):
+        """Return the contour of the card at x,y"""
+        for contour in all_contours:
+            if cv2.pointPolygonTest(contour, (x, y), False) >= 0:
 
+                # Obtenha as coordenadas e dimensões do retângulo delimitador do objeto
+                return cv2.boundingRect(contour)
     
+    def crop_image(self,raw_image: numpy.ndarray, contour):
+        """Crops the image around the given contour"""
+        x, y, w, h = contour
+        cropped_image = raw_image[y:y+h, x:x+w]
+        return cropped_image
+        
